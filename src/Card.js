77c91e34b7beb81495dc3d047391138f6b5cbd45
John Onaeko <jonaeko11@gmail.com>
2023-06-29 21:25:57 +0100

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const card = () => {
+    return(
+        <div>
+            <img src="https://robohash.org/testtest?size=200x200" alt="robotphoto"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>janedoe@gmail.com</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default card; 
